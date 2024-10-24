17a14b8978b5dd6bcd3b8b1c4d716ccf1efc3be3
C12H22O12 <director20844@gmail.com>
2024-10-24 11:37:44 +0900

    feat: Carousel

--- src/component/carousel.tsx
+++ src/component/carousel.tsx
@@ -1,3 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+import { DefaultCard } from "./default-card";
+
 export const Carousel = (): JSX.Element => {
-  return <>Carousel</>;
+  const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const [visiable, setVisiable] = useState<number[]>([]);
+
+  const handleCardClick = (idx: number) => {
+    const selectedCard = cardRefs.current[idx];
+    if (selectedCard) {
+      selectedCard.scrollIntoView({ behavior: "smooth", inline: "center" });
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const cardIdx = Number(entry.target.getAttribute("data-index"));
+
+          if (entry.isIntersecting && entry.intersectionRatio === 1) {
+            setVisiable((prev) => [...prev, cardIdx]);
+          } else {
+            setVisiable((prev) => prev.filter((idx) => idx !== cardIdx));
+          }
+        });
+      },
+      { threshold: [0, 1] }
+    );
+
+    const currentRef = cardRefs;
+
+    currentRef.current.forEach((ref) => {
+      if (ref) observer.observe(ref);
+    });
+
+    return () => {
+      currentRef.current.forEach((ref) => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, []);
+
+  return (
+    <div className="w-full flex gap-4 overflow-auto">
+      {Array.from(Array(8), (_, idx) => (
+        <div
+          key={`default_card_carousel_${idx}`}
+          ref={(el) => {
+            cardRefs.current[idx] = el;
+          }}
+          data-index={idx}
+          onClick={() => handleCardClick(idx)}
+        >
+          <DefaultCard isVisible={visiable.includes(idx)}>
+            {idx + 1} card
+          </DefaultCard>
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/component/default-card.tsx
+++ src/component/default-card.tsx
@@ -1,7 +1,20 @@
-export const DefaultCard = (): JSX.Element => {
+import { PropsWithChildren } from "react";
+
+interface DefaultCardProps extends PropsWithChildren {
+  isVisible: boolean;
+}
+
+export const DefaultCard = ({
+  isVisible,
+  children,
+}: DefaultCardProps): JSX.Element => {
   return (
-    <div className="p-4 max-w-64 h-dvh mx-auto rounded-xl flex items-center space-x-4 border">
-      Default Card
+    <div
+      className={`p-4 w-64 min-w-64 mx-auto rounded-xl flex items-center space-x-4 border  transition-all ${
+        isVisible ? "bg-green-500" : "bg-red-500"
+      }`}
+    >
+      {children}
     </div>
   );
 };
