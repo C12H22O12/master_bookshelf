ef969c55cba47e3c0beaeb8f5c6e94c83861bdf1
C12H22O12 <director20844@gmail.com>
2025-01-09 17:56:47 +0900

    feat: 기본 색상 세팅 및 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,13 +11,21 @@ const config: Config = {
     extend: {
       width: {
         "4/5dvw": "40dvw",
+        "m-button": "283px",
+        "p-button": "253px",
       },
       height: {
         "4/5dvh": "80dvh",
+        "button": "43px",
       },
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        title: "var(--title)",
+        content: "var(--content)",
+        button: "var(--button-content)",
+        point: "var(--point)",
+        boardbg: "var(--boardbg)",
+        boardct: "var(--boardct)",
       },
     },
   },
