39705e54fee28191fe82ea63f11600afed8225c8
C12H22O12 <director20844@gmail.com>
2025-01-09 18:54:14 +0900

    feat: mobile button

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
+
 interface ButtonProps {
   text: string;
 }
 
 export const Button = ({ text }: ButtonProps) => {
   return (
-    <button className="w-p-button h-button rounded-xl bg-point text-button hover:bg-hover">
-      {text}
+    <button className="flex items-center justify-between md:justify-center w-m-button md:w-p-button h-button rounded-xl bg-point text-button overflow-hidden hover:bg-hover">
+      <Image
+        src="/danchung.png"
+        alt="단청 이미지"
+        width={39}
+        height={43}
+        className="md:hidden"
+      />
+      <div className="text-xl">{text}</div>
+      <Image
+        src="/danchung.png"
+        alt="단청 이미지"
+        width={39}
+        height={43}
+        className="rotate-180 md:hidden"
+      />
     </button>
   );
 };
